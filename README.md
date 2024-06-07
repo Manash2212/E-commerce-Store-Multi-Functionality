3ae6b04545e162aafb00fb145ce3f53fd1a54d5d
Manash <manashhalder2212@gmail.com>
2024-06-07 13:34:25 +0530

    Setup the routing Part

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# E-commerce-Store-Multi-Functionality
+# E-commerce-Store-With-Multi-Functionality
+
 A complete E-commerce project with the help of React, Strapi, Stripe, Firebase and so on...
+
+- Create Two folder api and client,
+- client is for react-app, so basically clone it from my git repo and it has already Tailwind css Configured.abs
+- Routing the Component.
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/products/:id", element: <Products /> },
+      { path: "/product/:id", element: <Product /> },
+    ],
+  },
+]);
 
 const App = () => {
   return (
-    <div className="text-3xl text-red-400 ">
-      <h1 className="bg-blue-300"> This is React App</h1>
+    <div>
+      <RouterProvider router={appRouter} />
     </div>
   );
 };
